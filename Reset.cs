4a403c541c77b6c1f8f90e4b9e6a588b6a881cda
yael <45328385+YAELMOR@users.noreply.github.com>
2019-01-09 15:51:12 +0200

    1.23
    Reset.Models-cs

---
+++ Reset.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCEventCalendar.Models
+{
+    public class Reset
+    {
+
+        [Required]
+        [RegularExpression("^[0-9]{8}$", ErrorMessage = "Please enter 8 digits in customer number")]
+        [Key]
+        public string ID { get; set; }
+        public string hash { get; set; }
+
+    }
+}
