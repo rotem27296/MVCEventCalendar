aea07b53b4ab20f0294f70fde2fcbe2c9d63f001
yael <45328385+YAELMOR@users.noreply.github.com>
2019-01-09 16:04:09 +0200

    1.41
    EventDal.Dal-cs

---
+++ EventDal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using MVCEventCalendar.Models;
+
+namespace MVCEventCalendar.DAL
+{
+    public class EventDal : DbContext
+    {
+        public DbSet<Models.Event> Event { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Models.Event>().ToTable("tblEvent");
+        }
+    }
+}
