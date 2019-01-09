ae9e70187e8f523312107d78202e42122e884397
yael <45328385+YAELMOR@users.noreply.github.com>
2019-01-09 16:07:41 +0200

    1.43
    UserDal.Dal-cs

---
+++ UsersDal.cs
@@ -0,0 +1,24 @@
+﻿using MVCEventCalendar.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCEventCalendar.Controllers
+{
+
+        public class UsersDal : DbContext
+        {
+            public DbSet<Users> User { get; set; }
+
+
+            protected override void OnModelCreating(DbModelBuilder modelBuilder)
+            {
+
+                base.OnModelCreating(modelBuilder);
+                modelBuilder.Entity<Users>().ToTable("tblUser");
+            }
+        }
+    
+}
