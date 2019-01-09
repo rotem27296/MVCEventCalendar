89a69558c8e76ee3455af56fe6affa56f0ebe9be
yael <45328385+YAELMOR@users.noreply.github.com>
2019-01-09 16:05:40 +0200

    1.42
    ResetDal.Dal-cs

---
+++ ResetDal.cs
@@ -0,0 +1,20 @@
+﻿using MVCEventCalendar.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Project.DAL
+{
+    public class ResetDal : DbContext
+    {
+        public DbSet<Reset> reset { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Reset>().ToTable("tblReset");
+        }
+    }
+}
