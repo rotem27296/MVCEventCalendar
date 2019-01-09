990d7a0e0562aee87ee010ff1f8f8b3bfd0e5dc0
yael <45328385+YAELMOR@users.noreply.github.com>
2019-01-09 15:46:09 +0200

    1.21
    Event.Models-cs

---
+++ Event.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCEventCalendar.Models
+{
+    public class Event
+    {
+        [Key]
+        public int EventID { get; set; }
+        public string Subject { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public System.DateTime Start { get; set; }
+        public Nullable<System.DateTime> End { get; set; }
+        public string ThemeColor { get; set; }
+        public bool IsFullDay { get; set; }
+
+        [RegularExpression("^[0-9]{8}$", ErrorMessage = "Please enter 8 digits in customer number")]
+        [Required]
+        public string User { get; set; }
+        public string Invite { get; set; }
+
+
+    }
+}
